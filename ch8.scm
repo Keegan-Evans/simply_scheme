129c8782bcf6b19f016425a083f507c4cd209f2b
Keegan Evans <keegan.evans@gmail.com>
2020-05-01 12:55:09 -0700

    problems 8.1 and 8.2 complete

---
+++ ch8.scm
@@ -0,0 +1,35 @@
+; 8.1
+;'(a e i o u)
+;(every last '(algebra purple spaghetti tomato gnu))
+;
+;'()
+;(keep number? '(one two three four))
+;
+;0
+;(accumulate * '(6 7 13 0 9 42 17))
+;
+;#f
+;(member? 'h (keep vowel? '(t h r o a t)))
+;
+;(16 144 0)
+;(every square (keep even? '(87 4 7 12 0 5)))
+;
+;ai
+;(accumulate word (keep vowel? (every first '(and i love her))))
+;
+;Got all correct.
+
+; 8.2
+; keep
+; every
+; first
+; every last
+; accumulate word every last
+; every
+; accumulate
+
+0
+((repeated square 0) 25)
+
+'(go d sunshi)
+(every (repeated bl 2) '(good day sunshine))
