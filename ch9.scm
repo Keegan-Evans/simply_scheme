0cd895e3f90c0f9ef83f2f2f3845dc93959f9b48
Keegan Evans <keegan.evans@gmail.com>
2020-05-08 10:47:40 -0700

    up to 9.10 complete

---
+++ ch9.scm
@@ -0,0 +1,53 @@
+; 9.1
+; it will state it is a procedure
+; 34
+; '(yan etim ta lal)
+; an error stating the incorrect number of arguements
+
+; 9.2
+(define second (lambda (stuff) (first (bf stuff))))
+
+(define make-adder (lambda (num) (lambda (x) (+ num x))))
+
+; 9.3
+; it returns last of a sentence by by using accumulate to look at the first word and the second word in a sentence then keeping only the second, it continues doing this will all of the other words in the sentence until only the last word is left.
+
+; 9.4
+(define (who sent)
+  (every 
+    (lambda (person) (se person sent))
+    '(pete roger john keith)))
+
+; 9.5 
+(define (prepend-every prefix sent)
+  (every
+    (lambda (wd) (word prefix wd))
+    sent))
+
+; 9.6
+(define (sentence-version fun)
+  (lambda (sent) (every fun sent)))
+
+; 9.7
+(define (letterwords letter sent)
+  (keep (lambda (wd) (member? letter wd)) sent))
+
+; 9.8
+;(define (hang-letter letter guesses)
+;  (if (member? letter guesses)
+;      letter
+;      '_))
+
+(define (hang secret_word guesses)
+  (every (lambda (letter) (if (member? letter guesses) letter '_)) 
+	 secret_word))
+
+
+; 9.9
+(define (common-words sent1 sent2)
+  (keep (lambda (wd) (member? wd sent2)) sent1))
+
+; 9.10
+(define (my_appearances arg1 arg2)
+  (count
+    (keep (lambda (letter) (equal? letter arg1)) arg2)))
