4a423d642bb3cfa4f6938f8d9a8d9cf2f1ed0f0e
Keegan Evans <keegan.evans@gmail.com>
2020-05-22 12:38:24 -0700

    finished all 11 except 11.4, another hilarious random and tangental question

--- ch11.scm
+++ ch11.scm
@@ -31,3 +31,41 @@
 	((equal? 'um (first sent))
 	 (+ 1 (count-ums (bf sent))))
 	(else (count-ums (bf sent)))))
+
+; 11.3
+(define (unspell-letter letter)
+  (cond ((member? letter 'abc) 2)
+        ((member? letter 'def) 3)
+        ((member? letter 'ghi) 4)
+	((member? letter 'jkl) 5)  
+	((member? letter 'mno) 6) 
+	((member? letter 'prs) 7) 
+	((member? letter 'tuv) 8) 
+	((member? letter 'wxy) 9) 
+	(else 0)))
+
+(define (phone-unspell wd)
+  (if (= (count wd) 1)
+      (unspell-letter wd)
+      (word (unspell-letter (first wd)) 
+	    (phone-unspell (bf wd)))))
+
+; 11.5
+(define (initials sent)
+  (if (empty? sent)
+      '()
+      (se (first (first sent)) 
+	  (initials (bf sent)))))
+
+; 11.6
+(define (countdown num)
+  (if (= num 0)
+      'BLASTOFF!
+      (se num (countdown (- num 1)))))
+
+; 11.7
+(define (copies num wd)
+  (if (= num 0)
+      '()
+      (se wd 
+	  (copies (- num 1) wd))))
